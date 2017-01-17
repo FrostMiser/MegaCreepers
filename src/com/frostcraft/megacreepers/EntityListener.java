fe7ec4f1667ffe8be6aa253217f2f9c6808aef0d
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-16 20:49:50 -0600

    Add main class and entity listener.

---
+++ src/com/frostcraft/megacreepers/EntityListener.java
@@ -0,0 +1,40 @@
+package com.frostcraft.megacreepers;
+
+
+import javax.persistence.Entity;
+
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+
+public class EntityListener implements Listener {
+	
+	@EventHandler
+	public void onEntitySpawn(EntitySpawnEvent event) {
+		if (event.getEntity().getType().equals(EntityType.CREEPER)) {
+			Creeper creeper = (Creeper) event.getEntity();
+			creeper.setPowered(true);
+		}
+	}
+	
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent event) {
+		if (event.getEntityType().equals(EntityType.CREEPER)) {
+			event.getLocation().getWorld().createExplosion(event.getLocation(), 10);			
+		}
+	}	
+	
+	public void onEntitySpawn(CreatureSpawnEvent event) {
+		if (!event.getEntity().getType().equals(EntityType.CREEPER) && event.getSpawnReason().equals(SpawnReason.CUSTOM)) {
+			Creeper creeper = (Creeper) event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.CREEPER);
+			creeper.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(100.0);
+			creeper.setHealth(100.0);
+		}
+	}
+}
---
+++ src/com/frostcraft/megacreepers/MegaCreepers.java
@@ -0,0 +1,25 @@
+package com.frostcraft.megacreepers;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.ChatColor;
+
+
+public class MegaCreepers extends JavaPlugin {
+
+	
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new EntityListener(),this);
+		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[MegaCreepers] Plugin Enabled.");
+		return;
+			
+	}
+
+	public void onDisable() {
+		this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "[MegaCreepers] Plugin Disabled.");
+	}
+}
